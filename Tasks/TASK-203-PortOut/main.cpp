7795c82679a2afd29c85fa8804135b5b8e887ec9
David Hendy <64068682+LockdownMadness@users.noreply.github.com>
2023-02-20 12:23:51 +0000

    TASK 203 - Completed
    Worked out PortOut and corresponding digit can be used to turn it on and off

--- Tasks/TASK-203-PortOut/main.cpp
+++ Tasks/TASK-203-PortOut/main.cpp
@@ -4,18 +4,29 @@
 #define TRAF_GRN1_PIN PC_6
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
-#define LEDMASK 0b0000000001001100
+
+
+
+#define LEDMASK1 0b0000000001001100 //binary corisponds to the pin number, so 2nd/3rd and 6th pins are 1's!!!!
+#define LEDMASK2 0b0100000010000001   //with this is toggles off TRAF_RED!_PIN - PC_PC2 as its in 2nd slot
+
 // Objects
-//BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
-PortOut leds(PortC, LEDMASK);
+//BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN, PB_0, PB_7, PB_14);
+PortOut leds1(PortC, LEDMASK1);
+PortOut leds2(PortB, LEDMASK2);
 
 int main()
 {
     while (true) {
-        leds = 0;   //Binary 000
+        //leds1 = 0;   //Binary 000
+        //leds2 = 0;
+
+        leds1 = leds1^LEDMASK1;
+        leds2 = leds2^LEDMASK2;
         wait_us(500000);
-        leds = 0xFF;   //Binary 11111111
-        wait_us(500000);    
+        //leds1 = 0xFF;
+        //leds2 = 0xFF;   //Binary 11111111
+        //wait_us(500000);    
     }
 }
 
--- Tasks/Task-202-BusOut/main.cpp
+++ Tasks/Task-202-BusOut/main.cpp
@@ -12,17 +12,12 @@ using namespace uop_msb;
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
 
-#define TRAF_GRN2_PIN PC_9
-#define TRAF_YEL2_PIN PC_8
-#define TRAF_RED2_PIN PC_7
-
-
 // Objects
 // DigitalOut grn(TRAF_GRN1_PIN);
 // DigitalOut yel(TRAF_YEL1_PIN);
 // DigitalOut red(TRAF_RED1_PIN,1);
-BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
-BusInOut traffic2(TRAF_GRN2_PIN, TRAF_YEL2_PIN, TRAF_RED2_PIN);
+BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN, PB_0, PB_7, PB_14); //the order in here determins the binary to turn it on!!!
+//BusOut leds(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 LCD_16X2_DISPLAY lcd;
 
 
@@ -34,49 +29,46 @@ int main()
         //leds = 7;   //Binary 111
         //wait_us(500000);    
     //}
-   // traffic1 = 0b111;
-    traffic2.output();
-    traffic2.mode(PinMode::OpenDrainNoPull);
 
 
     while (true) {
 
 
 
+        int n;
+        float b;
         
-        volatile int n;
-        volatile float b;    
-        for (n=1; n<=15; n = n+1) {
+        for (n=1; n<=32; n = n+1) {
             printf("n=%d\n", n);
             lcd.cls();
             lcd.printf("Real Num: %d\n", n);
+            //uint32_t binary = *reinterpret_cast<uint32_t*>(&n);
 
-                if(n==1){
-                    lcd.locate(1, 0);   //Row 1, Col 0
-                    lcd.printf(" Bin Num:00001");
-                    }else if (n==2){
-                        lcd.locate(1, 0);   //Row 1, Col 0
-                        lcd.printf(" Bin Num:00010");
-                            }else if (n==3){
-                                lcd.locate(1, 0);   //Row 1, Col 0
-                                lcd.printf(" Bin Num:00011");
-                                    }else if (n==4){
-                                        lcd.locate(1, 0);   //Row 1, Col 0
-                                        lcd.printf(" Bin Num:00100");
-                                            }else if (n==5){
-                                                lcd.locate(1, 0);   //Row 1, Col 0
-                                                lcd.printf(" Bin Num:00101");
-                                                    }else if (n==6){
-                                                        lcd.locate(1, 0);   //Row 1, Col 0
-                                                        lcd.printf(" Bin Num:00110");
-                                                            }else if (n==7){
-                                                                lcd.locate(1, 0);   //Row 1, Col 0
-                                                                lcd.printf(" Bin Num:00111");
-                                                            };
+                //if(n==1){
+                    //lcd.locate(1, 0);   //Row 1, Col 0
+                    //lcd.printf(" Bin Num:00001");
+                    //}else if (n==2){
+                        //lcd.locate(1, 0);   //Row 1, Col 0
+                        //lcd.printf(" Bin Num:00010");
+                            //}else if (n==3){
+                                //lcd.locate(1, 0);   //Row 1, Col 0
+                                //lcd.printf(" Bin Num:00011");
+                                    //}else if (n==4){
+                                        //lcd.locate(1, 0);   //Row 1, Col 0
+                                        //lcd.printf(" Bin Num:00100");
+                                            //}else if (n==5){
+                                                //lcd.locate(1, 0);   //Row 1, Col 0
+                                                //lcd.printf(" Bin Num:00101");
+                                                    //}else if (n==6){
+                                                        //lcd.locate(1, 0);   //Row 1, Col 0
+                                                        //lcd.printf(" Bin Num:00110");
+                                                           // }else if (n==7){
+                                                                //lcd.locate(1, 0);   //Row 1, Col 0
+                                                                //lcd.printf(" Bin Num:00111");
+                                                            //};
 
             leds = n;
-            traffic2 = n;
-            wait_us(2000000);
+            wait_us(200000);
         }
 
     
